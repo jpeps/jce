6b780b42d8ffefe7bec2b1326f755de7e1046b04
jeldridgeTS <jeldridge@turbosquid.com>
2016-08-12 15:16:01 -0500

    Set port dyanmically

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 3000))
+
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
   res.render('landing')
 })
 
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("Running app")
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 })
